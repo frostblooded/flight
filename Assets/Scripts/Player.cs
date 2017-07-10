a1276003ee4e76111820586653e0ac0b9cd5d0f0
Nikolay Danailov <nikolay_danailov@yahoo.com>
2017-07-10 14:37:32 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    public int JUMP_FORCE = 10;
+    
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Make bird jump up if any key is pressed
+        if (Input.anyKey)
+        {
+            Vector3 movementVector = new Vector3(0, JUMP_FORCE, 0);
+            rb2D.AddForce(movementVector);
+        }
+    }
+}
