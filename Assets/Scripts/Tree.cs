65967ddfb552ccfd9ef9e81e1bbd9c97f0df0371
Nikolay Danailov <nikolay_danailov@yahoo.com>
2017-07-11 14:01:46 +0300

    Make the Y of the bottom component use the distance between the components correctly

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -9,7 +9,7 @@ public class Tree : MonoBehaviour
     public float MoveSpeed = 0.2f;
     public float TopComponentMinY = 3.0f;
     public float TopComponentMaxY = 7.0f;
-    public float DistanceBetweenComponentsY = 10.0f;
+    public float DistanceBetweenComponentsY = 3.5f;
     public float DestroyTreesLeftOfX = -20.0f;
 
     public GameObject treeComponentPrefab;
@@ -27,8 +27,17 @@ public class Tree : MonoBehaviour
 
     private void Start()
     {
+        Vector3 treeComponentSize = treeComponentPrefab.GetComponent<Renderer>().bounds.size;
+
         float topTreeY = Random.Range(TopComponentMinY, TopComponentMaxY);
-        float bottomTreeY = topTreeY - DistanceBetweenComponentsY;
+
+        // To get what the Y of the bottom component should be, we first substract
+        // the distance that we need between the components, but we also need to substract
+        // half of the component's height which is taken into account because the Y
+        // of the top component is actually its center. Then we substract half of the 
+        // component's height again because the case is the same when it comes to
+        // the bottom component. Ultimately we just substract the component's height once.
+        float bottomTreeY = topTreeY - DistanceBetweenComponentsY - treeComponentSize.y;
         
         GameObject topTreeComponent = Instantiate(treeComponentPrefab, this.transform);
         GameObject bottomTreeComponent = Instantiate(treeComponentPrefab, this.transform);
