a78178a3fac1e6ed12ac0db93df4c73273a7ea89
Nikolay Danailov <nikolay_danailov@yahoo.com>
2017-07-11 13:23:49 +0300

    Make the whole tree into one object, which is made of smaller tree components

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,60 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
     public float SecondsBetweenTrees = 1;
-    public float TreesSpeed = 0.2f;
-
     public GameObject treePrefab;
-
-    public float TreeCreationX = 20;
-    public float TopTreeMinY = 3.0f;
-    public float TopTreeMaxY = 7.0f;
-    public float DistanceBetweenTreesY = 10.0f;
-    public float DestroyTreesLeftOfX = -20.0f;
 
     private DateTime lastTreesCreatedAt = DateTime.Now;
     private Transform treeHolder;
 
-    void CreateTreeAt(float x)
-    {
-        // Get a random position for the top tree and spawn
-        // the rest of the tree parts based on that
-        float topTreeY = UnityEngine.Random.Range(TopTreeMinY, TopTreeMaxY);
-        float bottomTreeY = topTreeY - DistanceBetweenTreesY;
-
-        Vector3 topTreePosition = new Vector3(x, topTreeY);
-        Vector3 bottomTreePosition = new Vector3(x, bottomTreeY);
-
-        Instantiate(treePrefab, topTreePosition, Quaternion.identity, treeHolder);
-        Instantiate(treePrefab, bottomTreePosition, Quaternion.identity, treeHolder);
-    }
-
-    void CreateTrees()
+    void CreateTree()
     {
         // If enought seconds have passed since the last tree was created,
         // create a new tree now
         if(lastTreesCreatedAt.AddSeconds(SecondsBetweenTrees) < DateTime.Now)
         {
-            CreateTreeAt(TreeCreationX);
+            Vector3 newTreePosition = new Vector2(Tree.CreationX, 0);
+            Instantiate(treePrefab, newTreePosition, Quaternion.identity, treeHolder);
+
             lastTreesCreatedAt = DateTime.Now;
-        }
-    }
-
-    void MoveTrees()
-    {
-        // Move all trees to the right
-        GameObject[] trees = GameObject.FindGameObjectsWithTag("Tree");
-
-        foreach(GameObject tree in trees)
-        {
-            tree.transform.position -= Vector3.right * TreesSpeed;
-
-            // Destroy tree if it went off the screen
-            if (tree.transform.position.x < DestroyTreesLeftOfX)
-            {
-                Destroy(tree.gameObject);
-            }
         }
     }
 
@@ -66,7 +30,6 @@ public class GameController : MonoBehaviour {
     }
 
     void Update () {
-        CreateTrees();
-        MoveTrees();
+        CreateTree();
 	}
 }
---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public static float CreationX = 20;
+
+    public float MoveSpeed = 0.2f;
+    public float TopComponentMinY = 3.0f;
+    public float TopComponentMaxY = 7.0f;
+    public float DistanceBetweenComponentsY = 10.0f;
+    public float DestroyTreesLeftOfX = -20.0f;
+
+    public GameObject treeComponentPrefab;
+
+    public void Move()
+    {
+        this.transform.position -= Vector3.right * MoveSpeed;
+
+        // Destroy component if it went off the screen
+        if (this.transform.position.x < DestroyTreesLeftOfX)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        float topTreeY = Random.Range(TopComponentMinY, TopComponentMaxY);
+        float bottomTreeY = topTreeY - DistanceBetweenComponentsY;
+        
+        GameObject topTreeComponent = Instantiate(treeComponentPrefab, this.transform);
+        GameObject bottomTreeComponent = Instantiate(treeComponentPrefab, this.transform);
+
+        // Move the x of the components' relative position to zero, so
+        // that they appear at the same x as their parent
+        topTreeComponent.transform.localPosition = new Vector2(0, topTreeY);
+        bottomTreeComponent.transform.localPosition = new Vector2(0, bottomTreeY);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+}
